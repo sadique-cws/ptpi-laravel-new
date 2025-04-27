3ff4feed4b30c8bed45bbed68cf2a6abbe6ad6a8
rahulxdev0 <rk854327@gmail.com>
2025-04-27 23:40:17 +0530

    adding columns in user modal

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('age')->nullable();
+            $table->enum('gender', ['male', 'female', 'other'])->nullable(); 
+            $table->enum('role', ['teacher', 'admin', 'recruiter', 'exam-setter'])->default('teacher');
+            $table->enum('religion', ['Hindu', 'Muslim', 'Other'])->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
