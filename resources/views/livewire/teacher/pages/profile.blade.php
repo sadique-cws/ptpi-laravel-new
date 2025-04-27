8e0fb6634aea0c9ebdd8b2ab9c08c6c012c12c6b
rahulxdev0 <rk854327@gmail.com>
2025-04-27 22:12:43 +0530

    adding personal profile

--- resources/views/livewire/teacher/pages/profile.blade.php
+++ resources/views/livewire/teacher/pages/profile.blade.php
@@ -1,7 +1,116 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
-    <div class="">
-        <h1 class="text-2xl font-bold">Profile</h1>
-        <p class="text-gray-500">Welcome to your profile!</p>
+    <div class="p-5 mt-2 flex flex-col gap-1 border rounded-lg">
+        <section class="Toastify" aria-live="polite" aria-atomic="false" aria-relevant="additions text"
+            aria-label="Notifications Alt+T"></section>
+        <h2 class="text-xl font-semibold mb-4 text-gray-600 flex items-center gap-1"><svg stroke="currentColor"
+                fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em"
+                xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"></path>
+            </svg> Basic Informations</h2>
+        <div class="flex justify-between items-center py-2 ">
+            <div class="flex  md:items-center">
+                <div class="hidden md:block">
+                    <p class="text-gray-700 font-medium w-24 md:w-40 truncate">Profile:</p>
+                </div>
+                <div class="flex flex-col items-center md:flex-row md:items-start w-80 mt-1 mb-3 md:mb-1">
+                    <div class=""><img src="https://api.ptpinstitute.com/media/profile_pics/momo.jpeg"
+                            alt="Profile" class="w-16 h-16 rounded-full object-cover"><button
+                            class="mt-2 text-sm text-[#3E98C7] bg-transparent hover:underline md:hidden">Edit
+                            Profile</button></div>
+                </div>
+            </div>
+            <div class="flex space-x-2"><button class="text-gray-500 rounded-full">
+                    <div class="text-[#3E98C7] hover:bg-none mr-4 hidden md:block">edit profile</div>
+                </button></div>
+        </div>
+        <hr>
+        <div class="flex justify-between items-center py-2 ">
+            <div class="flex  md:items-center">
+                <div class="flex items-center">
+                    <p class="text-gray-700 font-medium w-24 md:w-40 truncate">Name:</p>
+                </div>
+                <p class="text-gray-600">Aman Kumar</p>
+            </div>
+        </div>
+        <hr>
+        <div class="flex justify-between items-center py-2 ">
+            <div class="flex  md:items-center">
+                <div class="flex items-center">
+                    <p class="text-gray-700 font-medium w-24 md:w-40 truncate">Email Adds:</p>
+                </div>
+                <p class="text-gray-600">testing123@gmail.com</p>
+            </div>
+        </div>
+        <hr>
+        <div class="flex justify-between items-center py-2 ">
+            <div class="flex  md:items-center">
+                <div class="flex items-center">
+                    <p class="text-gray-700 font-medium w-24 md:w-40 truncate">Contact No:</p>
+                </div>
+                <p class="text-gray-600">7004672980</p>
+            </div>
+            <div class="flex space-x-2"><button class="text-gray-500 rounded-full">
+                    <div
+                        class="flex items-center justify-center text-gray-700 font-medium text-sm px-4 border-[1.5px] border-gray-200 py-1 rounded-lg md:mr-4">
+                        Edit</div>
+                </button></div>
+        </div>
+        <hr>
+        <div class="flex justify-between items-center py-2 ">
+            <div class="flex  md:items-center">
+                <div class="flex items-center">
+                    <p class="text-gray-700 font-medium w-24 md:w-40 truncate">Language:</p>
+                </div>
+                <p class="text-gray-600">English</p>
+            </div>
+            <div class="flex space-x-2"><button class="text-gray-500 rounded-full">
+                    <div
+                        class="flex items-center justify-center text-gray-700 font-medium text-sm px-4 border-[1.5px] border-gray-200 py-1 rounded-lg md:mr-4">
+                        Edit</div>
+                </button></div>
+        </div>
+        <hr>
+        <div class="flex justify-between items-center py-2 ">
+            <div class="flex  md:items-center">
+                <div class="flex items-center">
+                    <p class="text-gray-700 font-medium w-24 md:w-40 truncate">Gender:</p>
+                </div>
+                <p class="text-gray-600">male</p>
+            </div>
+            <div class="flex space-x-2"><button class="text-gray-500 rounded-full">
+                    <div
+                        class="flex items-center justify-center text-gray-700 font-medium text-sm px-4 border-[1.5px] border-gray-200 py-1 rounded-lg md:mr-4">
+                        Edit</div>
+                </button></div>
+        </div>
+        <hr>
+        <div class="flex justify-between items-center py-2 ">
+            <div class="flex  md:items-center">
+                <div class="flex items-center">
+                    <p class="text-gray-700 font-medium w-24 md:w-40 truncate">Marital Status:</p>
+                </div>
+                <p class="text-gray-600">single</p>
+            </div>
+            <div class="flex space-x-2"><button class="text-gray-500 rounded-full">
+                    <div
+                        class="flex items-center justify-center text-gray-700 font-medium text-sm px-4 border-[1.5px] border-gray-200 py-1 rounded-lg md:mr-4">
+                        Edit</div>
+                </button></div>
+        </div>
+        <hr>
+        <div class="flex justify-between items-center py-2 ">
+            <div class="flex  md:items-center">
+                <div class="flex items-center">
+                    <p class="text-gray-700 font-medium w-24 md:w-40 truncate">Religion:</p>
+                </div>
+                <p class="text-gray-600">hindu</p>
+            </div>
+            <div class="flex space-x-2"><button class="text-gray-500 rounded-full">
+                    <div
+                        class="flex items-center justify-center text-gray-700 font-medium text-sm px-4 border-[1.5px] border-gray-200 py-1 rounded-lg md:mr-4">
+                        Edit</div>
+                </button></div>
+        </div>
+        <hr>
     </div>
 </div>
