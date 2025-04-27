18185e1b5b126b405ff66c19e4df1b53c3d239b8
rahulxdev0 <rk854327@gmail.com>
2025-04-27 21:20:14 +0530

    adding teacher layout sidebar

---
+++ app/Livewire/Teacher/Components/Sidebar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Teacher\Components;
+
+use Livewire\Component;
+
+class Sidebar extends Component
+{
+    public function render()
+    {
+        return view('livewire.teacher.components.sidebar');
+    }
+}
