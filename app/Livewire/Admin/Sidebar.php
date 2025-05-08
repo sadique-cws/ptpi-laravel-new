597d02a0602205eb97aef28b6b453b1ecd3b8dc7
rahulxdev0 <rk854327@gmail.com>
2025-05-08 17:31:12 +0530

    adding logout in admin panel

--- app/Livewire/Admin/Sidebar.php
+++ app/Livewire/Admin/Sidebar.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire\Admin;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Sidebar extends Component
 {
+    public function adminLogout(){
+        Auth::logout();
+        session()->invalidate();
+        session()->regenerateToken();
+        return redirect()->route('login');
+    }
     public function render()
     {
         return view('livewire.admin.sidebar');
--- resources/views/livewire/admin/sidebar.blade.php
+++ resources/views/livewire/admin/sidebar.blade.php
@@ -102,7 +102,7 @@
 
     <!-- Logout Button -->
     <div class="absolute bottom-0 left-0 right-0 p-4 border-t border-gray-800">
-        <button @click="logout" class="flex items-center w-full p-2 rounded-lg hover:bg-gray-700 group">
+        <button wire:click="adminLogout" class="flex items-center w-full p-2 rounded-lg hover:bg-gray-700 group">
             <svg class="w-5 h-5 text-gray-400 group-hover:text-white" fill="currentColor" viewBox="0 0 20 20">
                 <path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path>
             </svg>
