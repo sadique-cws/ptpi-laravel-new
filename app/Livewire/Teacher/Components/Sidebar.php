ab9cf90deffe8d7234849090356884c037ff3c92
rahulxdev0 <rk854327@gmail.com>
2025-05-08 17:20:53 +0530

    logout done

--- app/Livewire/Teacher/Components/Sidebar.php
+++ app/Livewire/Teacher/Components/Sidebar.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire\Teacher\Components;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Sidebar extends Component
 {
+    public function logout(){
+        Auth::logout();
+        session()->invalidate();
+        session()->regenerateToken();
+        return redirect()->route('login');
+    }
     public function render()
     {
         return view('livewire.teacher.components.sidebar');
